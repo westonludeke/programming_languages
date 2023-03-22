9e4c6df46d5e37088c1f880a43e02aa6f5996491
westonludeke <westonludeke@gmail.com>
2023-03-21 19:10:15 -0500

    fixed intents in scripts file

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 const descriptions = document.querySelectorAll('.description');
-  const showMoreButtons = document.querySelectorAll('.show-more');
+const showMoreButtons = document.querySelectorAll('.show-more');
 
-  for (let i = 0; i < showMoreButtons.length; i++) {
-    showMoreButtons[i].addEventListener('click', () => {
-      const description = descriptions[i];
-      if (description.classList.contains('show-all')) {
-        description.classList.remove('show-all');
-        showMoreButtons[i].textContent = 'Show More';
-      } else {
-        description.classList.add('show-all');
-        showMoreButtons[i].textContent = 'Show Less';
-      }
-    });
-  }
+for (let i = 0; i < showMoreButtons.length; i++) {
+  showMoreButtons[i].addEventListener('click', () => {
+    const description = descriptions[i];
+    if (description.classList.contains('show-all')) {
+      description.classList.remove('show-all');
+      showMoreButtons[i].textContent = 'Show More';
+    } else {
+      description.classList.add('show-all');
+      showMoreButtons[i].textContent = 'Show Less';
+    }
+  });
+}
